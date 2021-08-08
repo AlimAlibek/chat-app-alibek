56edd87311b0aa9c0fb55f3ed792aad74b651659
AlimAlibek <talmbiev-alim@mail.ru>
2021-08-08 10:08:31 +0300

    init

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const http = require("http");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
@@ -9,10 +10,14 @@ app.use(express.json());
 
 if (process.env.NODE_ENV === "prduction") {
     app.use(express.static("../client/build"))
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "..", "client", "build", "index.html"));
+    })
 }
 
 app.get("/", (req, res) => {
-    res.status(200).send({message: "server is running"})
+    res.status(200).send({message: "server is running"});
 })
   
 // ======================================================================
